3ea17e41c9b1574ab9775fed068d68c449c77cb3
Marquis Bentley <livefreekai@icloud.com>
2020-10-17 12:15:09 -0700

    Make qbot send a hello message when a user connects

--- features/resume.js
+++ features/resume.js
@@ -4,6 +4,9 @@ module.exports = function(controller) {
     controller.on('connected', async(bot, message) => {
         await bot.reply(message, `Hello! My name is ${ resume.name }`);
     });
+    controller.on('welcome_back', async(bot, message) => {
+        await bot.reply(message, `Hello! My name is Marquis`);
+    });
     
     controller.hears('name','message,direct_message', async(bot, message) => {
         await bot.reply(message, `My name is ${resume.basics.name}`);
