0bfbd80f5a90e24b33b17a4995ba3e39a04d6e53
Marquis Bentley <livefreekai@icloud.com>
2020-10-17 12:57:45 -0700

    Add dialog to give out contact info

---
+++ features/contact_info.js
@@ -0,0 +1,35 @@
+const resume = require('./resume.json');
+
+module.exports = function(controller) {
+    
+    // LinkedIn
+    controller.hears('linkedin','message,direct_message', async(bot, message) => {
+        await bot.reply(message, {type: 'typing'});
+        setTimeout(async () => {
+            // will have to reset context because turn has now ended.
+            await bot.changeContext(message.reference);
+            await bot.reply(message, `Let's connect on LinkedIn. Here is my profile, ${resume.contact.linkedin}`);
+        }, 1000);
+    });
+    
+    // email
+    controller.hears('email','message,direct_message', async(bot, message) => {
+        await bot.reply(message, {type: 'typing'});
+        setTimeout(async () => {
+            // will have to reset context because turn has now ended.
+            await bot.changeContext(message.reference);
+            await bot.reply(message, `Feel free to reach out via email, ${resume.contact.email}.`);
+        }, 1000);
+    });
+    
+    // Github
+    controller.hears('github','message,direct_message', async(bot, message) => {
+        await bot.reply(message, {type: 'typing'});
+        setTimeout(async () => {
+            // will have to reset context because turn has now ended.
+            await bot.changeContext(message.reference);
+            await bot.reply(message, `Check out my github profile, ${resume.contact.github}.`);
+        }, 1000);
+    });
+    
+}
--- features/greeting.js
+++ features/greeting.js
@@ -1,8 +1,17 @@
 module.exports = function(controller) {
-    // const greetings = ['hello', 'hi', 'hey', 'hola']
+    
+    // user's first time with the bot trigger connected message
     controller.on('connected', async(bot, message) => {
         await bot.reply(message, `Hello! My name is Marquis, I'm a Software Engineer from Oakland, CA.`);
+        await bot.reply(message, {type: 'typing'});
+        setTimeout(async () => {
+            // will have to reset context because turn has now ended.
+            await bot.changeContext(message.reference);
+            await bot.reply(message, 'Care to know more?');
+        }, 1000);
     });
+
+    // if cookies are found, trigger welcome_back 
     controller.on('welcome_back', async(bot, message) => {
         await bot.reply(message, `Hello! My name is Marquis, I'm a Software Engineer from Oakland, CA.`);
         await bot.reply(message, {type: 'typing'});
