95f7f7265b39f89ae3e41869ce8ac363bec617a0
Marquis Bentley <livefreekai@icloud.com>
2020-10-17 16:19:01 -0700

    Adjust the length of the typing animations depending on length of response

--- features/basics.js
+++ features/basics.js
@@ -9,7 +9,7 @@ module.exports = function(controller) {
             // will have to reset context because turn has now ended.
             await bot.changeContext(message.reference);
             await bot.reply(message, `I am ${resume.basics.age} years old.`);
-        }, 1000);
+        }, 900);
     });
 
     // location
@@ -19,17 +19,17 @@ module.exports = function(controller) {
             // will have to reset context because turn has now ended.
             await bot.changeContext(message.reference);
             await bot.reply(message, `I am located in ${resume.basics.location.city}. Although I'm open to remote work and possibly relocation for the right company.`);
-        }, 1000);
+        }, 2000);
     });
     
     // name
-    controller.hears('locate','message,direct_message', async(bot, message) => {
-        await bot.reply(message, {type: 'typing'});
-        setTimeout(async () => {
-            // will have to reset context because turn has now ended.
-            await bot.changeContext(message.reference);
-            await bot.reply(message, `My name is Marquis Bentley.`);
-        }, 1000);
-    });
+    // controller.hears('locate','message,direct_message', async(bot, message) => {
+    //     await bot.reply(message, {type: 'typing'});
+    //     setTimeout(async () => {
+    //         // will have to reset context because turn has now ended.
+    //         await bot.changeContext(message.reference);
+    //         await bot.reply(message, `My name is Marquis Bentley.`);
+    //     }, 1000);
+    // });
     
 }
