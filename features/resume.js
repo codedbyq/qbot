9aa523a35f995e71326903939ca11bc88b31fc9c
Marquis Bentley <livefreekai@icloud.com>
2020-10-17 12:00:46 -0700

    Remove sample dialog, and created a default response for things the bot does't know

--- features/resume.js
+++ features/resume.js
@@ -1,16 +1,16 @@
 const resume = require('./resume.json');
 module.exports = function(controller) {
-    
-    controller.on('channel_join', async(bot, message) => {
+    // const greetings = ['hello', 'hi', 'hey', 'hola']
+    controller.on('connected', async(bot, message) => {
         await bot.reply(message, `Hello! My name is ${ resume.name }`);
     });
-
+    
     controller.hears('name','message,direct_message', async(bot, message) => {
         await bot.reply(message, `My name is ${resume.basics.name}`);
     });
 
-    // controller.hears('hmmm','message,direct_message', async(bot, message) => {
-    //     await bot.reply(message, 'hmmm this is a tricky one!');
-    // });
+    controller.hears('experience','message,direct_message', async(bot, message) => {
+        await bot.reply(message, "Would you like to know what I'm working on currently, or an overview of my recent experience?");
+    });
 
 }
--- features/sample_echo.js
+++ features/sample_echo.js
@@ -5,16 +5,16 @@
 
 module.exports = function(controller) {
 
-    controller.hears('sample','message,direct_message', async(bot, message) => {
-        await bot.reply(message, 'I heard a sample message.');
-    });
+    // controller.hears('sample','message,direct_message', async(bot, message) => {
+    //     await bot.reply(message, 'I heard a sample message.');
+    // });
 
-    controller.hears('hmmm','message,direct_message', async(bot, message) => {
-        await bot.reply(message, 'hmmm this is a tricky one');
-    });
+    // controller.hears('hmmm','message,direct_message', async(bot, message) => {
+    //     await bot.reply(message, 'hmmm this is a tricky one');
+    // });
 
     controller.on('message,direct_message', async(bot, message) => {
-        await bot.reply(message, `Echo: ${ message.text }`);
+        await bot.reply(message, `Hmm ${ message.text }? I'm not sure what that is.`);
     });
 
 }
