f0bdff2bb467864089eaff18c93609c295c7ed3c
Marquis Bentley <livefreekai@icloud.com>
2020-10-18 14:41:42 -0700

    Change the wording in the intro message

--- features/greeting.js
+++ features/greeting.js
@@ -2,7 +2,7 @@ module.exports = function(controller) {
     
     // user's first time with the bot trigger connected message
     controller.on('connected', async(bot, message) => {
-        await bot.reply(message, `Hello! My name is QBot, digital avatar for my creator Marquis Bentley. I'm a Software Engineer in the San Francisco Bay Area, currently open to new opportunities, and looking to grow my network. Come get to know me better!`);
+        await bot.reply(message, `Hello! My name is QBot, the digital avatar for my creator Marquis Bentley. I'm a Software Engineer in the San Francisco Bay Area, currently open to new opportunities, and looking to grow my network. Come get to know me better!`);
         await bot.reply(message, {type: 'typing'});
         setTimeout(async () => {
             // will have to reset context because turn has now ended.
@@ -42,7 +42,7 @@ module.exports = function(controller) {
 
     // if cookies are found, trigger welcome_back 
     controller.on('welcome_back', async(bot, message) => {
-        await bot.reply(message, `Hello! My name is QBot, digital avatar for my creator Marquis Bentley. I'm a Software Engineer in the San Francisco Bay Area, currently open to new opportunities, and looking to grow my network. Come get to know me better!`);
+        await bot.reply(message, `Hello! My name is QBot, the digital avatar for my creator Marquis Bentley. I'm a Software Engineer in the San Francisco Bay Area, currently open to new opportunities, and looking to grow my network. Come get to know me better!`);
         await bot.reply(message, {type: 'typing'});
         setTimeout(async () => {
             // will have to reset context because turn has now ended.
