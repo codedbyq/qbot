f7255faa20fff96bed566dbe49748bd838748f7b
Marquis Bentley <livefreekai@icloud.com>
2020-10-18 14:47:10 -0700

    Remove unused dialog

--- features/basics.js
+++ features/basics.js
@@ -22,14 +22,5 @@ module.exports = function(controller) {
         }, 2000);
     });
     
-    // name
-    // controller.hears('locate','message,direct_message', async(bot, message) => {
-    //     await bot.reply(message, {type: 'typing'});
-    //     setTimeout(async () => {
-    //         // will have to reset context because turn has now ended.
-    //         await bot.changeContext(message.reference);
-    //         await bot.reply(message, `My name is Marquis Bentley.`);
-    //     }, 1000);
-    // });
     
 }
--- features/practics.js
+++ features/practics.js
@@ -8,12 +8,4 @@ module.exports = function(controller) {
         }
     });
 
-    controller.hears('hmmm','message,direct_message', async(bot, message) => {
-        await bot.reply(message, 'hmmm this is a tricky one!');
-    });
-
-    controller.on('message,direct_message', async(bot, message) => {
-        await bot.reply(message, `Echo: ${ message.text }`);
-    });
-
 }
