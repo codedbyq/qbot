176b697525616d9a574b14e6d4decca112381535
Marquis Bentley <livefreekai@icloud.com>
2020-10-18 15:20:30 -0700

    Write dialog to talk about working for App Academy

--- features/experience.js
+++ features/experience.js
@@ -22,24 +22,35 @@ module.exports = function(controller) {
                 quick_replies: [
                     {
                         title: 'App Academy',
-                        payload: 'Tell me more about your time with App Academy.',
+                        payload: 'Tell me more about your time working with App Academy.',
                     },
                     {
                         title: 'Project Happy',
-                        payload: 'Tell me more about your time with Project Happy.',
+                        payload: 'Tell me more about your time working with Project Happy.',
                     },
                     {
                         title: 'Enjoy Technology',
-                        payload: 'Tell me more about your time with Enjoy.',
+                        payload: 'Tell me more about your time working with Enjoy.',
                     },
                     {
                         title: 'Best Buy',
-                        payload: 'Tell me more about your time with Best Buy.',
+                        payload: 'Tell me more about your time working with Best Buy.',
                     }
                 ]
             });
         }, 2000);
     });
+
+    controller.hears('working with App Academy', 'message, direct_message', async(bot, message) => {
+        await bot.reply(message, {type: 'typing'});
+        setTimeout(async () => {
+            // will have to reset context because turn has now ended.
+            const job = resume.work[0]
+            await bot.changeContext(message.reference);
+            await bot.reply(message, `I worked for ${job.company} as a ${job.position} where ${job.summary}.`);
+        }, 1000);
+    });
+
 }
 
 // ask for a follow up about a particular job
