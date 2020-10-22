be51c231a3e782f3f2fa080f48cbe1ac85bd8513
Marquis Bentley <livefreekai@icloud.com>
2020-10-22 08:23:48 -0700

    Add name dialog

--- features/basics.js
+++ features/basics.js
@@ -14,6 +14,16 @@ module.exports = function(controller) {
         }, 1000);
     });
 
+    // name
+    controller.hears('name','message,direct_message', async(bot, message) => {
+        await bot.reply(message, {type: 'typing'});
+        setTimeout(async () => {
+            // will have to reset context because turn has now ended.
+            await bot.changeContext(message.reference);
+            await bot.reply(message, `My name is ${resume.basics.name}.`);
+        }, 900);
+    });
+
     // age
     controller.hears('age','message,direct_message', async(bot, message) => {
         await bot.reply(message, {type: 'typing'});
