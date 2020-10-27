265db0863af007223eb4c9b15066b913972db7c6
Marquis Bentley <livefreekai@icloud.com>
2020-10-26 22:01:57 -0700

    Add prompt to learn more after asking about the basics

--- features/basics.js
+++ features/basics.js
@@ -12,6 +12,37 @@ module.exports = function(controller) {
             await bot.changeContext(message.reference);
             await bot.reply(message, `I am a ${basics.age} year old ${basics.title} in ${basics.location.city}.`);
         }, 1000);
+        await bot.reply(message, {type: 'typing'});
+        setTimeout(async () => {
+            // will have to reset context because turn has now ended.
+            await bot.changeContext(message.reference);
+            await bot.reply(message, {
+                text: "Anything else you'd like to know about me?",
+                // provide user with premade questions
+                quick_replies: [
+                    {
+                        title: 'Work',
+                        payload: 'Tell me more about your work experience.',
+                    },
+                    {
+                        title: 'Education',
+                        payload: 'Tell me more about your education.',
+                    },
+                    {
+                        title: 'Projects',
+                        payload: 'Tell me more about your projects.',
+                    },
+                    {
+                        title: 'Skills',
+                        payload: 'Tell me more about your skills.',
+                    },
+                    {
+                        title: 'Interests',
+                        payload: 'Tell me more about your personal interests.',
+                    }
+                ]
+            });
+        }, 1000);
     });
 
     // name
